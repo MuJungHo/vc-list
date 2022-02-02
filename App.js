f9891a5bc0a564d77f352a8fa62c1a44fb4dafe8
MuJungHo <ande1218@gmail.com>
2022-02-02 22:10:28 +0800

    Fetch data from coingecko api.

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, View, VirtualizedList, StyleSheet, Text, StatusBar } from 'react-native';
 
-export default function App() {
+
+const Item = ({ name }) => (
+  <View style={styles.item}>
+    <Text style={styles.name}>{name}</Text>
+  </View>
+);
+
+const App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [dataSource, setData] = useState([]);
+  const [market, setMarket] = useState('usd');
+  const getItem = (data, index) => ({ ...dataSource[index] })
+  const getItemCount = (data) => 20;
+  useEffect(() => {
+    fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${market}`)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, [market]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{ flex: 1, padding: 24 }}>
+        {isLoading ? <Text>Loading...</Text> :
+          (<VirtualizedList
+            data={dataSource}
+            initialNumToRender={4}
+            renderItem={({ item }) => <Item name={item.name} />}
+            keyExtractor={item => item.id}
+            getItemCount={getItemCount}
+            getItem={getItem}
+          />)}
+      </View>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    marginTop: StatusBar.currentHeight,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    height: 150,
     justifyContent: 'center',
+    marginVertical: 8,
+    marginHorizontal: 16,
+    padding: 20,
+  },
+  name: {
+    fontSize: 32,
   },
 });
+
+export default App;
